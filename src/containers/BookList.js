dd088230f1e66cc46e7b45a6a65e5eac5fe22dfc
Ruby <c19890611@gmail.com>
2017-10-03 15:02:30 -0500

    redux or react?

---
+++ src/containers/BookList.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { selectBook }  from '../actions/index';
+import { bindActionCreators } from 'redux';
+
+
+class BookList extends Component {
+  render() {
+    //must create a map function here to return the following:
+        const item = this.props.books.map((book) => {
+            console.log(book)
+            return (
+                <li
+                    key={book.id}
+                    onClick={() => this.props.selectBook(book)}
+                    className="list-group-item">
+                    {book.title}
+                </li>
+            )
+        })
+    return (
+
+        <div class="jumbotron jumbotron-fluid col-sm-6" >
+            <div class="container">
+                  <ul className="list-group">
+                    {item}
+                  </ul>
+            </div>
+        </div>
+
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  console.log("mapstate", state.books);
+  //what is returned will show up as props inside of BookList
+  //this gives you access to books in props.. (books would be good for mapping)
+  return {
+    books: state.books,
+  };
+}
+
+//anything returned from this function will end up as props on
+//BookList Container.
+function mapDispatchToProps(dispatch) {
+  //whenever selectBook is called, result should be passed to
+  //all of the reducers. (flows through dispatch function -- like a funnel)
+    return bindActionCreators({ selectBook: selectBook }, dispatch)
+}
+//connect all functions to container component
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import '../styles/index.css';
+import App from './components/App';
+import registerServiceWorker from './registerServiceWorker';
+import {Provider} from 'react-redux'
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from './reducers/index'
+
+
+const createStoreWithMiddleware = applyMiddleware()(createStore)
+
+
+ReactDOM.render(
+
+<Provider store={createStoreWithMiddleware(rootReducer)}>
+
+  <App />
+
+</Provider>
+
+
+, document.getElementById('root'));
+registerServiceWorker();
